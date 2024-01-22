0a0adcb49b5c6f25f5aae39d09ab5ee3b06fbaaa
unknown <mohammadparsapourr@gmail.com>
2024-01-22 01:07:52 -0800

    commit

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from blogs.models import Post
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('title','counted_views','published_date','status','created_date')
+    list_filter = ('status','published_date',)
+    search_fields = ['title', 'content']
+admin.site.register(Post,PostAdmin)
