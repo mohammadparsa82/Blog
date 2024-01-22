c4c51e489e412097c4e8006cba0f3f58b61f238e
unknown <mohammadparsapourr@gmail.com>
2024-01-22 00:18:56 -0800

    commit

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post (models.Model):
+    title =  models.CharField(max_length=100)
+    content = models.TextField()
+    counted_views = models.IntegerField()
+    published_date = models.DateTimeField()
+    status = models.BooleanField()
+    created_date = models.DateTimeField(auto_now_add = True)
+    update_date = models.DateTimeField(auto_now = True)
