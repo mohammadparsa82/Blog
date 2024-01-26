5d69c1833a5a1a946107e04f7ecae7b730cd1747
unknown <mohammadparsapourr@gmail.com>
2024-01-26 10:06:20 -0800

    commit

--- blogs/admin.py
+++ blogs/admin.py
@@ -3,7 +3,7 @@ from blogs.models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
-    list_display = ('title','counted_views','published_date','status','created_date')
-    list_filter = ('status','published_date',)
+    list_display = ('title','author','counted_views','published_date','status','created_date')
+    list_filter = ('status','published_date','author',)
     search_fields = ['title', 'content']
 admin.site.register(Post,PostAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post (models.Model):
+    author = models.ForeignKey(User ,on_delete=models.CASCADE, null=True)
     title =  models.CharField(max_length=100)
     content = models.TextField()
     counted_views = models.IntegerField()
