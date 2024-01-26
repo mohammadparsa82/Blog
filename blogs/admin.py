c5ce384655f4482b6a0cc159282c6cc91c417dc3
unknown <mohammadparsapourr@gmail.com>
2024-01-26 13:21:07 -0800

    commit

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from blogs.models import Post
+from blogs.models import Post ,Category
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     list_display = ('title','author','counted_views','published_date','status','created_date', 'image' )
     list_filter = ('status','published_date','author',)
     search_fields = ['title', 'content']
+
+admin.site.register(Category)
 admin.site.register(Post,PostAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length= 150)
+
+    def __str__(self):
+        return self.name
+
+
 class Post (models.Model):
     author = models.ForeignKey(User ,on_delete=models.CASCADE, null=True)
     title =  models.CharField(max_length=100)
@@ -11,7 +18,7 @@ class Post (models.Model):
     created_date = models.DateTimeField(auto_now_add = True)
     update_date = models.DateTimeField(auto_now = True)
     image = models.ImageField(upload_to= 'blog/', default='blog/default.jpg')
-
+    category = models.ManyToManyField(Category)
     class Meta:
         ordering = ['-created_date']
     #show title and id 
