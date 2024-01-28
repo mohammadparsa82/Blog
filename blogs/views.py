4e97389c3bfaf5843db0fcfa22d637c1f65bd94f
unknown <mohammadparsapourr@gmail.com>
2024-01-28 09:20:59 -0800

    commit

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , get_object_or_404
 from blogs.models import Post
+from django.core.paginator import Paginator ,EmptyPage ,PageNotAnInteger
 
 
 def blogs_view(request, **kwargs):
@@ -9,6 +10,15 @@ def blogs_view(request, **kwargs):
 
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
+        
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
         
     context = {'posts': posts}
     return render(request, 'blogs/blog-home.html', context)
