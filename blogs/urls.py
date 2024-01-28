e198217e17fec3abfb0dd9d49081173dd6de72e8
unknown <mohammadparsapourr@gmail.com>
2024-01-28 04:50:25 -0800

    commit

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,5 @@ urlpatterns =[
     path('', views.blogs_view, name='index'),
     path('<int:pid>', views.blogs_single, name='single'),
     path('category/<str:cat_name>', views.blogs_view, name='category'),
+    path('author/<str:author_username>', views.blogs_view, name='author'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render , get_object_or_404
 from blogs.models import Post
 
 
-def blogs_view(request,cat_name=None):
+def blogs_view(request, **kwargs):
     posts = Post.objects.filter(status = 1)
-    if cat_name:
-        posts = posts.filter(category__name=cat_name)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts': posts}
     return render(request, 'blogs/blog-home.html', context)
 
