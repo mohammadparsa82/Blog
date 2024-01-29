d51639753265b47b95ce5f408a16bda90ca9c3ff
unknown <mohammadparsapourr@gmail.com>
2024-01-29 12:22:24 -0800

    commit

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse , HttpResponseRedirect
 from website.forms import ContactForm , NewsLettersForms
+from django.contrib import messages
+
 
 def index_view(request):
     return render(request , 'website/index.html')
@@ -13,8 +15,10 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submited successfully')
+        else:
+            messages.add_message(request,messages.ERROR,'your ticket didnt submited')
     form = ContactForm()
-    
     return render(request , 'website/contact.html', {'form':form})
 
 def newsletter(request):
