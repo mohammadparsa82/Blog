0a2b5803b4ed9f03e19994ecf66fefb1569d3195
unknown <mohammadparsapourr@gmail.com>
2024-01-29 09:14:53 -0800

    commit

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
+from website.forms import ContactForm
 
 def index_view(request):
     return render(request , 'website/index.html')
@@ -8,4 +9,10 @@ def about_view(request):
     return render(request , 'website/about.html')
 
 def contact_view(request):
-    return render(request , 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm()
+    
+    return render(request , 'website/contact.html', {'form':form})
