623b7393039111ef176885dd5d02ba45868f6569
unknown <mohammadparsapourr@gmail.com>
2024-02-03 00:07:05 -0800

    commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'django.contrib.humanize',
+    'django.contrib.sites',
 ]
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
