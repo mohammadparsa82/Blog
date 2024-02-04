3db4d29817c4256b7ae269fb7fc2da54e308c2df
unknown <mohammadparsapourr@gmail.com>
2024-02-04 12:59:48 -0800

    commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'robots',
     "debug_toolbar",
     "taggit",
+    'django_summernote',
+    
 ]
 
 #site framework
@@ -56,6 +58,43 @@ SITE_ID = 2
 # robots
 ROBOTS_USE_HOST = True
 ROBOTS_USE_SITEMAP = True
+
+#summernote config
+SUMMERNOTE_THEME = 'bs4'
+SUMMERNOTE_CONFIG = {
+    # Using SummernoteWidget - iframe mode, default
+    'iframe': True,
+
+    # Or, you can set it to `False` to use SummernoteInplaceWidget by default - no iframe mode
+    # In this case, you have to load Bootstrap/jQuery sources and dependencies manually.
+    # Use this when you're already using Bootstrap/jQuery based themes.
+    'iframe': True,
+
+    # You can put custom Summernote settings
+    'summernote': {
+        # As an example, using Summernote Air-mode
+        'airMode': False,
+
+        # Change editor size
+        'width': '100%',
+        'height': '480',
+
+
+
+        # Toolbar customization
+        # https://summernote.org/deep-dive/#custom-toolbar-popover
+        'toolbar': [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['fontname', ['fontname']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['fullscreen', 'codeview', 'help']],
+        ],
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -153,3 +192,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [  
     "127.0.0.1",
 ]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- blogs/admin.py
+++ blogs/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from blogs.models import Post ,Category
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     date_hierarchy = 'created_date'
     list_display = ('title','author','counted_views','published_date','status','created_date', 'image' )
     list_filter = ('status','published_date','author',)
     search_fields = ['title', 'content']
-
+    summernote_fields = ('content',)
 admin.site.register(Category)
 admin.site.register(Post,PostAdmin)
